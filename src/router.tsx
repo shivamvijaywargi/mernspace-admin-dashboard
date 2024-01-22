e84c75af9758ce72e7e55be4784c7f2a17007dc0
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-01-22 22:40:51 +0530

    feat: Update router configuration, persist login on page refresh

--- src/router.tsx
+++ src/router.tsx
@@ -3,25 +3,32 @@ import HomePage from "./pages/home-page";
 import LoginPage from "./pages/login/login-page";
 import Dashboard from "./layouts/dashboard";
 import NonAuth from "./layouts/non-auth";
+import Root from "./layouts/root";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
+    element: <Root />,
     children: [
       {
         path: "/",
-        element: <HomePage />,
+        element: <Dashboard />,
+        children: [
+          {
+            path: "/",
+            element: <HomePage />,
+          },
+        ],
       },
-    ],
-  },
-  {
-    path: "/auth",
-    element: <NonAuth />,
-    children: [
       {
-        path: "login",
-        element: <LoginPage />,
+        path: "/auth",
+        element: <NonAuth />,
+        children: [
+          {
+            path: "login",
+            element: <LoginPage />,
+          },
+        ],
       },
     ],
   },
