42d8374db67dd509e33077d82163e43c88146d29
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-03-08 22:49:26 +0530

    feat: add breadcrumb

--- src/pages/users/users-page.tsx
+++ src/pages/users/users-page.tsx
@@ -1,5 +1,16 @@
+import { Breadcrumb } from "antd";
+import { RightOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <>
+      <Breadcrumb
+        items={[{ title: <Link to={"/"}>Dashboard</Link> }, { title: "Users" }]}
+        separator={<RightOutlined />}
+      />
+    </>
+  );
 };
 
 export default UsersPage;
