6c9114cd0cf18f2630baa6e8bda61aa9a4e76842
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-03-09 00:04:04 +0530

    chore: remove unused import

--- src/pages/users/users-page.tsx
+++ src/pages/users/users-page.tsx
@@ -1,4 +1,4 @@
-import { Breadcrumb, Space, Table } from "antd";
+import { Breadcrumb, Table } from "antd";
 import { RightOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
