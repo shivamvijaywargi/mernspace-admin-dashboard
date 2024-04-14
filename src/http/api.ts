c6204ef67ff8aa8318bcf9ae9d7c00e2119620d0
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-04-14 12:24:50 +0530

    feat: add suffix since an API gateway was introduced✅

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,18 +1,22 @@
 import { ICreateUser, ICredentials } from "../types";
 import { api } from "./client";
 
+export const AUTH_SERVICE = "/api/auth";
+// const CATALOG_SERVICE = "/api/catalog";
+
 // Auth Service
 export const login = (credentials: ICredentials) =>
-  api.post("/auth/login", credentials);
-export const self = () => api.get("/auth/self");
-export const logout = () => api.post("/auth/logout");
+  api.post(`${AUTH_SERVICE}/auth/login`, credentials);
+export const self = () => api.get(`${AUTH_SERVICE}/auth/self`);
+export const logout = () => api.post(`${AUTH_SERVICE}/auth/logout`);
 
 // Users API endpoints
 export const getUsers = (queryString: string) =>
-  api.get(`/users?${queryString}`);
-export const createUser = (user: ICreateUser) => api.post("/users", user);
+  api.get(`${AUTH_SERVICE}/users?${queryString}`);
+export const createUser = (user: ICreateUser) =>
+  api.post(`${AUTH_SERVICE}/users`, user);
 export const updateUser = (id: number, user: ICreateUser) =>
-  api.patch(`/users/${id}`, user);
+  api.patch(`${AUTH_SERVICE}/users/${id}`, user);
 
 // Tenants/Restaurants API endpoints
-export const getRestaurants = () => api.get("/tenants");
+export const getRestaurants = () => api.get(`${AUTH_SERVICE}/tenants`);
